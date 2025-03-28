34f1e3443891591665aa102ae7cc82e448c473dd
MaryG78 <105908365+MaryG78@users.noreply.github.com>
2025-03-28 10:45:40 +0100

    Désactivation de update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/GuiM78/P7Scoring
-git pull origin main
-touch /var/www/GuiM78_pythonanywhere_com_wsgi.py
+#cd /home/GuiM78/P7Scoring
+#git pull origin main
+#touch /var/www/GuiM78_pythonanywhere_com_wsgi.py
 
