ee980767af47694fa11a3fa88588c1c1b2581ca3
Marie Guigou <guigoumarie@gmail.com>
2025-05-07 11:42:23 +0200

    corrections

--- dashboard_scoring/visualizations.py
+++ dashboard_scoring/visualizations.py
@@ -50,7 +50,7 @@ def plot_score_gauge(score_data, threshold=SEUIL_METIER):
     )
 
     fig = apply_accessible_style(fig)
-    add_alt_text("Jauge de score indiquant la probabilité de défaut du client avec code couleur.")
+   #add_alt_text("Jauge de score indiquant la probabilité de défaut du client avec code couleur.")
     st.plotly_chart(fig, use_container_width=True)
 
 
